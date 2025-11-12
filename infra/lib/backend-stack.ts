a347becba070654bb4701cf56f13effaa3237857
mombeh nadine <nadinemombeh@gmail.com>
2025-11-12 13:57:53 +0100

    build the infrastructure for backend

--- infra/lib/backend-stack.ts
+++ infra/lib/backend-stack.ts
@@ -1,16 +1,57 @@
+// import * as cdk from "aws-cdk-lib";
+// import { Construct } from "constructs";
+// // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+// export class InfraStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     // The code that defines your stack goes here
+
+//     // example resource
+//     // const queue = new sqs.Queue(this, 'InfraQueue', {
+//     //   visibilityTimeout: cdk.Duration.seconds(300)
+//     // });
+//   }
+// }
+
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
 
-export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface BackendStackProps extends cdk.StackProps {
+  vpc?: ec2.IVpc;
+}
+
+export class BackendStack extends cdk.Stack {
+  public readonly apiUrlOutput: cdk.CfnOutput;
+
+  constructor(scope: Construct, id: string, props: BackendStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cluster = new ecs.Cluster(this, "ApiCluster", {
+      vpc: props.vpc,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const service = new ecsPatterns.ApplicationLoadBalancedFargateService(this, "ApiService", {
+      cluster,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset("../apps/api"),
+        containerPort: 4000,
+        environment: {
+          NODE_ENV: "production",
+        },
+      },
+      publicLoadBalancer: true,
+    });
+
+    this.apiUrlOutput = new cdk.CfnOutput(this, "ApiUrl", {
+      value: `http://${service.loadBalancer.loadBalancerDnsName}`,
+    });
   }
 }
