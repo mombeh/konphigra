f18ed54a01b4c4660eb2f1b23bd41e8f49435a7c
mombeh nadine <nadinemombeh@gmail.com>
2025-11-13 16:04:15 +0100

    configured my access keys to be able to deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 👈 new Next.js 13.4+ way to enable static export
-  distDir: "out", // optional, ensures CDK finds /out
+  output: "export",
+  distDir: "out",
 };
 
 export default nextConfig;
