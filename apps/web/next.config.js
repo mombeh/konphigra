235620883bcc54b96d3d1afd7eea76fa8a07862c
mombeh nadine <nadinemombeh@gmail.com>
2025-11-13 15:12:02 +0100

    installed the out folder in my web

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // 👈 new Next.js 13.4+ way to enable static export
+  distDir: "out", // optional, ensures CDK finds /out
+};
 
 export default nextConfig;
