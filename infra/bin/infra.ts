a25a5c702c5fd7f3e3c0185b8b5004984fd8a6d1
mombeh <nadinemombeh@gmail.com>
2025-11-24 10:56:14 +0100

    imported the database stack in the infra.ts

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { NetworkStack } from "../lib/network-stack";
+import { DatabaseStack } from "../lib/database-stack";
 
 const app = new cdk.App();
 
-// FIRST STACK ONLY
-new NetworkStack(app, "Network-Stack", {
+const network = new NetworkStack(app, "Network-Stack", {
+  env: { account: "945799872792", region: "eu-north-1" },
+});
+
+new DatabaseStack(app, "Database-Stack", {
+  vpc: network.vpc,
   env: { account: "945799872792", region: "eu-north-1" },
 });
