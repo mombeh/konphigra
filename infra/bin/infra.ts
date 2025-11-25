4d05cb0b36c70aa90705fa04ee3ddcf7f192970b
mombeh <nadinemombeh@gmail.com>
2025-11-25 13:36:29 +0100

    create the stack for authentication and deployed on cloud

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+
 import { NetworkStack } from "../lib/network-stack";
 import { DatabaseStack } from "../lib/database-stack";
+import { AuthStack } from "../lib/auth-stack";
 
 const app = new cdk.App();
 
@@ -15,4 +17,8 @@ const network = new NetworkStack(app, "Network-Stack", { env });
 const database = new DatabaseStack(app, "Database-Stack", {
   env,
   vpc: network.vpc,
+});
+
+const auth = new AuthStack(app, "Auth-Stack", {
+  env,
 });
