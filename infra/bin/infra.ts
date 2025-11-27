5e48c62c7658b5512536986f655412458ac256e6
mombeh <nadinemombeh@gmail.com>
2025-11-27 14:38:08 +0100

    imported the frontendstack in the infra.ts file

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -6,6 +6,7 @@ import { DatabaseStack } from "../lib/database-stack";
 import { AuthStack } from "../lib/auth-stack";
 import { MonitoringStack } from "../lib/monitoring-stack";
 import { BackendStack } from "../lib/backend-stack";
+import { FrontendStack } from "../lib/frontend-stack";
 
 const app = new cdk.App();
 
@@ -27,6 +28,11 @@ const auth = new AuthStack(app, "Auth-Stack", {
 
 const monitoring = new MonitoringStack(app, "Monitoring-Stack", {
   env,
+});
+
+const frontend = new FrontendStack(app, "Frontend-Stack", {
+  env,
+  apiUrl: "https://placeholder.com",
 });
 
 const backend = new BackendStack(app, "Backend-Stack", {
