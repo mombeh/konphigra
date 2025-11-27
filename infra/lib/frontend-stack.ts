a2f15b69126791674e621d6666f5103b05b884bc
mombeh <nadinemombeh@gmail.com>
2025-11-27 14:35:44 +0100

    created a frontend-stack

--- infra/lib/frontend-stack.ts
+++ infra/lib/frontend-stack.ts
@@ -1,43 +1,43 @@
-// import * as cdk from "aws-cdk-lib";
-// import * as s3 from "aws-cdk-lib/aws-s3";
-// import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-// import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-// import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
-// import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { Construct } from "constructs";
 
-// interface FrontendStackProps extends cdk.StackProps {
-//   apiUrl: string;
-// }
+interface FrontendStackProps extends cdk.StackProps {
+  apiUrl: string;
+}
 
-// export class FrontendStack extends cdk.Stack {
-//   constructor(scope: Construct, id: string, props: FrontendStackProps) {
-//     super(scope, id, props);
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: FrontendStackProps) {
+    super(scope, id, props);
 
-//     const siteBucket = new s3.Bucket(this, "WebBucket", {
-//       publicReadAccess: true,
-//       blockPublicAccess: new s3.BlockPublicAccess({
-//         blockPublicAcls: false,
-//         blockPublicPolicy: false,
-//         ignorePublicAcls: false,
-//         restrictPublicBuckets: false,
-//       }),
-//       websiteIndexDocument: "index.html",
-//       websiteErrorDocument: "index.html",
-//     });
+    const siteBucket = new s3.Bucket(this, "WebBucket", {
+      publicReadAccess: true,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }),
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+    });
 
-//     const distribution = new cloudfront.Distribution(this, "WebDistribution", {
-//       defaultBehavior: { origin: new origins.S3Origin(siteBucket) },
-//     });
+    const distribution = new cloudfront.Distribution(this, "WebDistribution", {
+      defaultBehavior: { origin: new origins.S3Origin(siteBucket) },
+    });
 
-//     new s3deploy.BucketDeployment(this, "DeployWebsite", {
-//       sources: [s3deploy.Source.asset("../apps/web/out")],
-//       destinationBucket: siteBucket,
-//       distribution,
-//       distributionPaths: ["/*"],
-//     });
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("../apps/web/out")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
 
-//     new cdk.CfnOutput(this, "FrontendUrl", {
-//       value: `https://${distribution.domainName}`,
-//     });
-//   }
-// }
+    new cdk.CfnOutput(this, "FrontendUrl", {
+      value: `https://${distribution.domainName}`,
+    });
+  }
+}
